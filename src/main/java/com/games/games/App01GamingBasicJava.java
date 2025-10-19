51c83ca4197618380ddb4b6e949ab87afb6b941a
Diya Salam <sdiya.salam.github@gmail.com>
2025-10-19 10:27:51 -0400

    add - Spring context and bean configuration

--- src/main/java/com/games/games/App01GamingBasicJava.java
+++ src/main/java/com/games/games/App01GamingBasicJava.java
@@ -5,12 +5,13 @@ import com.games.games.game.MarioGame;
 import com.games.games.game.PacManGame;
 import com.games.games.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 	public static void main(String[] args) {
 //		var game = new MarioGame();
-		var game = new PacManGame();
+		var game = new PacManGame(); //Object creation
 //		var game = new SuperContraGame();
-		var gameRunner = new GameRunner(game);
+		var gameRunner = new GameRunner(game); // Object creation + Wiring of dependency
+		//Game is a dependency of GameRunner
 		gameRunner.run();
 	}
 }
---
+++ src/main/java/com/games/games/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.games.games;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+	
+	@Bean
+	public String name() {
+		return "John";
+	}
+}
