3198cd4c788995fd9b4578602bcc149ebc934474
Diya Salam <sdiya.salam.github@gmail.com>
2025-10-19 09:46:40 -0400

    add

--- src/main/java/com/games/games/AppGamingBasicJava.java
+++ src/main/java/com/games/games/AppGamingBasicJava.java
@@ -2,11 +2,13 @@ package com.games.games;
 
 import com.games.games.game.GameRunner;
 import com.games.games.game.MarioGame;
+import com.games.games.game.PacManGame;
 import com.games.games.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 	public static void main(String[] args) {
-		var game = new MarioGame();
+//		var game = new MarioGame();
+		var game = new PacManGame();
 //		var game = new SuperContraGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
---
+++ src/main/java/com/games/games/game/PacManGame.java
@@ -0,0 +1,17 @@
+package com.games.games.game;
+
+public class PacManGame implements GamingConsole{
+	public void up() {
+		System.out.println("Up");
+	}
+	public void down() {
+		System.out.println("down");
+	}
+	public void left() {
+		System.out.println("left");
+	}
+	public void right() {
+		System.out.println("right");
+	}
+
+}
